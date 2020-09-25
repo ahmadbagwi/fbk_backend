7bbf14298e6c2895a6b0810fc9cdd15278ea146e
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-09-25 17:11:46 +0700

    share flash message Laravel ke frontend dengan Inertia

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Session;
+use Inertia\Inertia;
+use Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,49 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+      Inertia::share('flash', function () {
+        return [
+          'message' => Session::get('status'),
+        ];
+      });
+
+      Inertia::share('validasi', function () {
+        return [
+          'message' => Session::get('status'),
+        ];
+      });
+
+      // Inertia::share('errorBags', function () {
+      //   return [
+      //     'message' => Session::get('errors'),
+      //   ];
+      // });
+
+      // Inertia::share('errors', function () {
+      //   return Session::get('errors')
+      //           ? Session::get('errors')->getBag('default')->getMessages()
+      //           : (object) [];
+      // });
+
+      // Inertia::share([
+      //   'flash' => [
+      //     'message' => Session::get('status')
+      //   ],
+
+      //   'errors' => [
+      //     'message' => Session::get('errors')
+      //   ],
+      // ]);
+
+      Inertia::share('auth.user', function () {
+        if (Auth::user()) {
+          return [
+            'id' => Auth::user()->id,
+            'name' => Auth::user()->name,
+            'email' => Auth::user()->email,
+            'role' => Auth::user()->role
+          ];
+        }
+      });
     }
 }
