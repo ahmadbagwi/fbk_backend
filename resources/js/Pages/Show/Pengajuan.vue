f55cbbd43fc46a6026d9aa1aeac7ab469913b8ed
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-09-25 20:20:34 +0700

    ubah judul, link, ganti img src menjadi button download File

--- resources/js/Pages/Show/Pengajuan.vue
+++ resources/js/Pages/Show/Pengajuan.vue
@@ -2,7 +2,7 @@
   <app-layout>
     <template #header>
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        Data Pengajuan
+        Pengajuan
       </h2>
     </template>
 
@@ -17,11 +17,11 @@
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et aliquam libero. Donec iaculis commodo efficitur. Suspendisse aliquam mollis ipsum, eu dictum nunc fermentum a. Curabitur aliquam lacus nisl, ac rutrum lorem placerat vel. 
             </p>
-            <jet-button>
-              <inertia-link  href="/pengajuan/create" :active="$page.currentRouteName == 'biodata_create'">
+            <inertia-link  href="/pengajuan/create" :active="$page.currentRouteName == 'biodata_create'">
+              <jet-button>
                 Tambah/Ubah Data
-              </inertia-link >
-            </jet-button>
+              </jet-button>
+            </inertia-link >
           </template>
 
           <template #laman>
@@ -66,11 +66,21 @@
                   </tr>
                   <tr>
                     <td class="border px-4 py-2">RAB</td>
-                    <td class="border px-4 py-2"><a href="data.rab">File RAB</a></td>
+                    <td class="border px-4 py-2">
+                      <div class="input-group-btn">
+                        <a :href="data.rab">
+                        <button class="flex text-black-600 border border-blue-600 hover:bg-gray-200 px-4 py-2 rounded" >
+                          <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cloud-download-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 0a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 4.095 0 5.555 0 7.318 0 9.366 1.708 11 3.781 11H7.5V5.5a.5.5 0 0 1 1 0V11h4.188C14.502 11 16 9.57 16 7.773c0-1.636-1.242-2.969-2.834-3.194C12.923 1.999 10.69 0 8 0zm-.354 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V11h-1v3.293l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/>
+                          </svg>&nbsp;File
+                        </button>
+                        </a>
+                      </div>
+                    </td>
                   </tr>
                   <tr>
                     <td class="border px-4 py-2">Video</td>
-                    <td class="border px-4 py-2"><a href="data.video" v-if="data.video">Link Video</a></td>
+                    <td class="border px-4 py-2"><a :href="data.video" v-if="data.video" class="text-blue-600">{{ data.video }}</a></td>
                   </tr>
                 </div>
               </table>
