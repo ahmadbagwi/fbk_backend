34c9254ffeb5a8eb2a9a546a0756793361a5d436
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-09-25 17:15:06 +0700

    import vue, inertia untuk initial vue js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,26 @@
 require('./bootstrap');
 
-import Vue from 'vue';
+import Vue from 'vue'
 
-import { InertiaApp } from '@inertiajs/inertia-vue';
-import { InertiaForm } from 'laravel-jetstream';
-import PortalVue from 'portal-vue';
+import { InertiaApp } from '@inertiajs/inertia-vue'
+import { InertiaForm } from 'laravel-jetstream'
+import PortalVue from 'portal-vue'
+import {ServerTable, ClientTable, Event} from 'vue-tables-2'
+import wysiwyg from "vue-wysiwyg"
 
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
 Vue.use(PortalVue);
+Vue.use(ClientTable, {}, false, 'bootstrap4')
+Vue.use(wysiwyg, {
+  image: {
+    uploadURL: "/upload",
+    dropzoneOptions: {
+      headers: { 'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content') }
+    }
+  },
+   maxHeight: "500px",
+})
 
 const app = document.getElementById('app');
 
