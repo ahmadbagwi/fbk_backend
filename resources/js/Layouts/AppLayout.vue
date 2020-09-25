f148bcb7a3c1c9bb22cb74aa3e2184e0e2b8fd5c
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-09-25 17:13:13 +0700

    update layout tambah menu dan menu mobile

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -18,6 +18,32 @@
                                 Dashboard
                             </jet-nav-link>
                         </div>
+                        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <jet-nav-link href="/biodata/show" :active="$page.currentRouteName == 'biodata_show'">
+                                Biodata
+                            </jet-nav-link>
+                        </div>
+                        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <jet-nav-link href="/pengajuan/show" :active="$page.currentRouteName == 'pengajuan_show'">
+                                Pengajuan
+                            </jet-nav-link>
+                        </div>
+                        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <jet-nav-link href="/administrasi/show" :active="$page.currentRouteName == 'administrasi_show'">
+                                Administrasi
+                            </jet-nav-link>
+                        </div>
+                        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <jet-nav-link href="/Laporan/show" :active="$page.currentRouteName == 'laporan_show'">
+                                Laporan
+                            </jet-nav-link>
+                        </div>
+                        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            <jet-nav-link href="/filemanager" :active="$page.currentRouteName == 'filemanager'">
+                                File Manager
+                            </jet-nav-link>
+                        </div>
+                        
                     </div>
 
                     <!-- Settings Dropdown -->
@@ -111,6 +137,21 @@
                     <jet-responsive-nav-link href="/dashboard" :active="$page.currentRouteName == 'dashboard'">
                         Dashboard
                     </jet-responsive-nav-link>
+                    <jet-responsive-nav-link href="/biodata/show" :active="$page.currentRouteName == 'biodata_show'">
+                        Biodata
+                    </jet-responsive-nav-link>
+                    <jet-responsive-nav-link href="/pengajuan/show" :active="$page.currentRouteName == 'pengajuan_show'">
+                        Pengajuan
+                    </jet-responsive-nav-link>
+                    <jet-responsive-nav-link href="/administrasi/show" :active="$page.currentRouteName == 'administrasi_show'">
+                        Administrasi
+                    </jet-responsive-nav-link>
+                    <jet-responsive-nav-link href="/laporan/show" :active="$page.currentRouteName == 'laporan_show'">
+                        Laporan
+                    </jet-responsive-nav-link>
+                    <jet-responsive-nav-link href="/filemanager" :active="$page.currentRouteName == 'filemanager'">
+                        File Manager
+                    </jet-responsive-nav-link>
                 </div>
 
                 <!-- Responsive Settings Options -->
@@ -181,19 +222,44 @@
                 </div>
             </div>
         </nav>
+        <!-- <fade-transition origin="center" mode="out-in" :duration="250"> -->
+          <!-- Page Heading -->
+          <header class="bg-white shadow">
+              <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                  <slot name="header"></slot>
+                  <br>
+                  <!-- <div style="display: inline;">
+                  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
+                    <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
+                  </svg><h1></h1>
+                  </div> -->
 
-        <!-- Page Heading -->
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                <slot name="header"></slot>
-            </div>
-        </header>
+                  <div class="col-span-6 flex items-center bg-gray-500 text-white text-sm font-bold px-4 py-3" role="alert">
+                  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
+                    <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
+                  </svg>
+                  <p>&nbsp;{{ $page.auth.user.name }}</p>
+                  </div>
+                  <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert" v-if="$page.flash.message">
+                    <strong class="font-bold">Info!</strong>
+                    <span class="block sm:inline">{{ $page.flash.message }}</span>
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                      <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                    </span>
+                  </div>
+              </div>
+          </header>
 
-        <!-- Page Content -->
-        <main>
-            <slot></slot>
-        </main>
-
+          <!-- Page Content -->
+          
+          <main>
+              <slot></slot>
+          </main>
+        <!-- </fade-transition> -->
         <!-- Modal Portal -->
         <portal-target name="modal" multiple>
         </portal-target>
@@ -201,6 +267,7 @@
 </template>
 
 <script>
+    import {FadeTransition} from 'vue2-transitions'
     import JetApplicationLogo from './../Jetstream/ApplicationLogo'
     import JetApplicationMark from './../Jetstream/ApplicationMark'
     import JetDropdown from './../Jetstream/Dropdown'
@@ -216,12 +283,18 @@
             JetDropdownLink,
             JetNavLink,
             JetResponsiveNavLink,
+            FadeTransition
         },
 
         data() {
             return {
+                
                 showingNavigationDropdown: false,
             }
+        },
+
+        created() {
+          console.log(this.$page.currentRouteName)
         },
 
         methods: {
@@ -247,3 +320,6 @@
         }
     }
 </script>
+<style scoped>
+  
+</style>
