dfe043b7edeff9ea45ba790813205453bd28d7f1
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-09-25 17:13:49 +0700

    test props dan konten dinamis Dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,29 +1,57 @@
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
+  <!-- <fade-transition origin="center" mode="out-in" :duration="250">  -->
+  <app-layout>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
-                </div>
-            </div>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ konten }}
+      </h2>
+    </template>
+
+      <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+
+            <!-- <biodata v-if="this.konten == 'biodata'" /> -->
+
+
+            <welcome/>
+
+          </div>
         </div>
-    </app-layout>
+      </div>
+      <div v-if="lists">
+        <tr cellpadding="1" v-for="(list, index) in lists" :key="list.id">
+          <td> {{ index }} </td>
+          <td>{{ list }}</td>
+        </tr>
+      </div>
+  </app-layout>
+  <!-- </fade-transition>   -->
 </template>
 
 <script>
+    import {FadeTransition} from 'vue2-transitions'
     import AppLayout from './../Layouts/AppLayout'
     import Welcome from './../Jetstream/Welcome'
+    import Biodata from './../Form/Biodata'
 
     export default {
         components: {
             AppLayout,
             Welcome,
+            FadeTransition,
+            Biodata
         },
+        props: {
+          satu: null,
+          dua: null,
+          lists: {}
+        },
+        data() {
+          return {
+            konten: this.$page.currentRouteName
+          }
+        }
     }
 </script>
