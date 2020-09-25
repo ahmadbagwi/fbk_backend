892f55f891f3c9fe04ccfbbc64933f70fdc2737d
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-09-25 17:16:09 +0700

    test route api, walaupun auth dengan default laravel, request api harus dengan token

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('/testform', function (Request $request) {
+      $data = $request->input('input1');
+      return response()->json('halo ' .$data);
+    })->name('testform');
+});
