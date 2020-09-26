b50cf83f7532af872ed33e0e567c02467e7e0d2d
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-09-26 12:46:52 +0700

    tambah props role

--- resources/js/Pages/Show/Laporan.vue
+++ resources/js/Pages/Show/Laporan.vue
@@ -2,7 +2,7 @@
   <app-layout>
     <template #header>
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        Laporan
+        Laporan {{ role }}
       </h2>
     </template>
 
@@ -101,6 +101,7 @@
         props: {
           status: null,
           data: {},
+          role: null
         },
         components: {
             AppLayout,
