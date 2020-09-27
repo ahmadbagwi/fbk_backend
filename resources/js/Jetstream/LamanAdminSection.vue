97d04584f027af235a508904347cb02200def284
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-09-27 10:29:27 +0700

    buat admin section template

---
+++ resources/js/Jetstream/LamanAdminSection.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="md:grid md:grid-cols-1 md:gap-6">
+        
+        <div class="mt-5 md:mt-0 md:col-span-2">
+            
+          <div class="shadow overflow-hidden sm:rounded-md">
+              <div class="px-4 py-5 bg-white sm:p-6">
+                  <div class="grid grid-cols-1 gap-6">
+                      <slot name="laman-admin"></slot>
+                  </div>
+              </div>
+
+              <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6" v-if="hasActions">
+                  <slot name="actions"></slot>
+              </div>
+          </div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+    import JetSectionTitle from './SectionTitle'
+
+    export default {
+        components: {
+            JetSectionTitle,
+        },
+
+        computed: {
+            hasActions() {
+                return !! this.$slots.actions
+            }
+        }
+    }
+</script>
