2b02c4cacb4e343778380abacfc9b5592b3442ca
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-09-29 13:49:42 +0700

    update field judul

--- resources/js/Pages/Form/Laporan.vue
+++ resources/js/Pages/Form/Laporan.vue
@@ -55,7 +55,7 @@
 
                     <div class="col-span-6 sm:col-span-4">
                       <div class="input-group">
-                        <jet-label for="laporan20" value="Laporan 20%" />
+                        <jet-label for="laporan20" value="Laporan Akhir 20%" />
                         <div class="input-group-btn">
                           <button class="flex ext-blue-600 border border-blue-600 hover:bg-gray-200 px-4 py-2 rounded" @click.prevent="openFileManager2">
                             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-paperclip" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
