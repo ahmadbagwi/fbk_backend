49dd479dd4365a142155dc3cc4746fed9d684620
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-09-30 10:15:58 +0700

    ganti select menjadi radio button, berikan detail di label form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,17 +10,17 @@
             @csrf
 
             <div>
-                <x-jet-label value="{{ __('Name') }}" />
-                <x-jet-input class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-jet-label value="{{ __('Nama Lengkap (huruf tanpa angka dan karakter) ')}}" />
+                <x-jet-input class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Pandu Wijaya" />
             </div>
 
             <div class="mt-4">
                 <x-jet-label value="{{ __('Email') }}" />
-                <x-jet-input class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-jet-input class="block mt-1 w-full" type="email" name="email" :value="old('email')" required  />
             </div>
 
             <div class="mt-4">
-                <x-jet-label value="{{ __('Password') }}" />
+                <x-jet-label value="{{ __('Password (huruf angka minimal 6 karakter) ')}}" />
                 <x-jet-input class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
@@ -31,12 +31,21 @@
 
             <div class="mt-4">
               <x-jet-label value="Kegiatan" />
-                <select id="kegiatan" class="block mt-1 w-full" name="kegiatan" required>
+              <input type="radio" id="FBK" name="kegiatan" value="FBK" required>
+              <label for="FBK">FASILITASI BIDANG KEBUDAYAAN (FBK)</label><br>
+              <input type="radio" id="FKBM" name="kegiatan" value="FKBM">
+              <label for="FKBM">FASILITASI KOMUNITAS BUDAYA DI MASYARAKAT (FKBM)</label><br>
+              <input type="radio" id="FKK" name="kegiatan" value="FKK">
+              <label for="FKK">FASILITASI KEGIATAN KESENIAN (FKK)</label><br>
+              <input type="radio" id="FSK" name="kegiatan" value="FSK">
+              <label for="FSK">FASILITASI SARANA KESENIAN (FSK)</label><br>
+
+                <!-- <select id="kegiatan" class="block mt-1 w-full" name="kegiatan" required>
                   <option value="FBK">FASILITASI BIDANG KEBUDAYAAN (FBK)</option>
                   <option value="FKBM">FASILITASI KOMUNITAS BUDAYA DI MASYARAKAT (FKBM)</option>
                   <option value="FKK">FASILITASI KEGIATAN KESENIAN (FKK)</option>
                   <option value="FSK">FASILITASI SARANA KESENIAN (FSK)</option>
-                </select>
+                </select> -->
             </div>
 
             <div class="flex items-center justify-end mt-4">
