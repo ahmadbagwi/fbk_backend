10cbb60b9481febb9d9032646df3e33036a3c083
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-10-01 09:28:23 +0700

    hapus sisa vue transitions

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -257,7 +257,6 @@
                 </div>
             </div>
         </nav>
-        <!-- <fade-transition origin="center" mode="out-in" :duration="250"> -->
           <!-- Page Heading -->
           <header class="bg-white shadow">
               <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -293,7 +292,6 @@
           <main>
               <slot></slot>
           </main>
-        <!-- </fade-transition> -->
         <!-- Modal Portal -->
         <portal-target name="modal" multiple>
         </portal-target>
@@ -315,8 +313,7 @@
             JetDropdown,
             JetDropdownLink,
             JetNavLink,
-            JetResponsiveNavLink,
-            FadeTransition
+            JetResponsiveNavLink
         },
 
         data() {
