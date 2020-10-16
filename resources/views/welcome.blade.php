ddbfbe5d0a06a2887cac986fd59ae66954b7d71c
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-10-16 23:52:59 +0700

    update font, bg-box-landing pattern svg https://www.heropatterns.com/

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,10 @@
     body {
       font-family: 'Nunito';
     }
+    .bg-box-landing {
+      background-color: #DFDBE5;
+      background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='80' height='80' viewBox='0 0 80 80'%3E%3Cg fill='%231c5b85' fill-opacity='0.03'%3E%3Cpath fill-rule='evenodd' d='M11 0l5 20H6l5-20zm42 31a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM0 72h40v4H0v-4zm0-8h31v4H0v-4zm20-16h20v4H20v-4zM0 56h40v4H0v-4zm63-25a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm10 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM53 41a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm10 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm10 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-30 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-28-8a5 5 0 0 0-10 0h10zm10 0a5 5 0 0 1-10 0h10zM56 5a5 5 0 0 0-10 0h10zm10 0a5 5 0 0 1-10 0h10zm-3 46a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm10 0a3 3 0 1 0 0-6 3 3 0 0 0 0 6zM21 0l5 20H16l5-20zm43 64v-4h-4v4h-4v4h4v4h4v-4h4v-4h-4zM36 13h4v4h-4v-4zm4 4h4v4h-4v-4zm-4 4h4v4h-4v-4zm8-8h4v4h-4v-4z'/%3E%3C/g%3E%3C/svg%3E");
+    }
   </style>
 </head>
 <body class="antialiased">
@@ -31,22 +35,22 @@
           <img src="/storage/files/logo_kemdikbud.png" class="align-middle img-fluid">
         </div>
         <div class="col-10 inline align-middle justify-content-start text-left">
-          <h1 class="text-gray-700 mt-4 text-md align-middle ">Fasilitasi Bidang Kebudayaan</h1>
+          <h2 class="text-white font-bold mt-4 text-5xl align-middle ">Fasilitasi Bidang Kebudayaan</h2>
         </div>
       </div>
 
-      <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+      <div class="mt-8 bg-box-landing dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
         <div class="grid grid-cols-1 md:grid-cols-2">
           <div class="p-6">
             <div class="flex items-center">
               <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-book" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M1 2.828v9.923c.918-.35 2.107-.692 3.287-.81 1.094-.111 2.278-.039 3.213.492V2.687c-.654-.689-1.782-.886-3.112-.752-1.234.124-2.503.523-3.388.893zm7.5-.141v9.746c.935-.53 2.12-.603 3.213-.493 1.18.12 2.37.461 3.287.811V2.828c-.885-.37-2.154-.769-3.388-.893-1.33-.134-2.458.063-3.112.752zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
               </svg>
-              <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">FBK</a></div>
+              <div class="ml-4 text-lg leading-7 font-semibold underline text-gray-900 dark:text-white">FBK</div>
             </div>
 
             <div class="ml-12">
-              <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+              <div class="mt-2 text-gray-900 dark:text-gray-400 text-md">
                 Fasilitasi Bidang Kebudayaan adalah salah satu upaya Pemerintah melalui Direktorat Jenderal Kebudayaan Kementerian Pendidikan dan Kebudayaan mewujudkan strategi pemajuan kebudayaan.
               </div>
             </div>
@@ -58,11 +62,11 @@
                 <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h2.5a2 2 0 0 1 1.6.8L8 14.333 9.9 11.8a2 2 0 0 1 1.6-.8H14a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                 <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
               </svg>
-              <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel-news.com/" class="underline text-gray-900 dark:text-white">Informasi</a></div>
+              <div class="ml-4 text-lg leading-7 font-semibold underline text-gray-900 dark:text-white">Informasi</div>
             </div>
 
             <div class="ml-12">
-              <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+              <div class="mt-2 text-gray-900 dark:text-gray-400 text-md">
                 Pendaftaran telah ditutup.
               </div>
             </div>
@@ -78,7 +82,7 @@
             </div>
 
             <div class="ml-12">
-              <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+              <div class="mt-2 text-gray-900 dark:text-gray-400 text-md">
                 Direktorat Jenderal Kebudayaan<br>
                 Komplek Kementerian Pendidikan dan Kebudayaan<br>
                 Gd. E Lt. 4<br>
@@ -101,7 +105,7 @@
           <div class="row justify-content-start text-right">
               @auth
               <div class="col justify-content-start">
-                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                <div class="mt-2 text-gray-900 dark:text-gray-400 text-sm">
                   <div class="d-inline-block">
                     <a href="{{ url('/dashboard') }}">
                     <button type="button" class="p-2 m-2 bg-red-200 text-lg btn btn-sm btn-outline-secondary">
@@ -115,7 +119,7 @@
               </div>
               @else
               <div class="col justify-content-start">
-                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                <div class="mt-2 text-gray-900 dark:text-gray-400 text-lg">
                   <div class="d-inline-block">
                     <a href="{{ route('login') }}">
                     <button type="button" class="p-2 m-2 bg-red-200 text-lg btn btn-sm btn-outline-secondary">
@@ -129,10 +133,10 @@
               </div>
               @if (Route::has('register'))
               <div class="col justify-content-start text-left">
-                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                <div class="mt-2 text-gray-900 dark:text-gray-400 text-lg">
                   <div class="d-inline-block">
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">
-                    <button type="button" class="p-2 m-2 bg-red-200 text-lg btn btn-sm btn-outline-success">
+                    <a href="{{ route('register') }}" class="ml-4 text-gray-700 underline">
+                    <button type="button" class="p-2 m-2 bg-red-200 btn btn-sm btn-outline-success">
                       <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-plus-fill d-inline-block" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7.5-3a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
                       </svg>&nbsp; Daftar
