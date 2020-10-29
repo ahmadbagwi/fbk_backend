33daa29f6ee2accaf77a975d9935cd02974b8203
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-10-29 10:52:28 +0700

    update style button, buat button-attach

--- resources/js/Jetstream/Button.vue
+++ resources/js/Jetstream/Button.vue
@@ -1,16 +1,33 @@
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+    <!-- <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
         <slot></slot>
-    </button>
+      </button> -->
+  <button :type="type" class="inline-flex items-center px-4 py-2 my-1 bg-green-600 border-b-2 border-green-800 rounded-lg text-md text-white tracking-widest hover:bg-orange-700 hover:shadow-lg active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+    <slot></slot>
+  </button>
 </template>
 
 <script>
-    export default {
-        props: {
-            type: {
-                type: String,
-                default: 'submit',
-            },
-        }
+  export default {
+    props: {
+      type: {
+        type: String,
+        default: 'submit',
+      },
     }
+  }
 </script>
+<style scoped>
+  .bg-biru {
+    background-color: #1D7CBF!important;
+  }
+
+  .bg-oren {
+    background-color: #F7A31C;
+  }
+
+  .text-oren {
+    color: #F7A31C;
+    font-weight: 700;
+  }
+</style>
---
+++ resources/js/Jetstream/ButtonAttach.vue
@@ -0,0 +1,31 @@
+<template>
+  <button :type="type" class="inline-flex ibg-blue-500 hover:bg-orange-500 hover:shadow-md text-md text-white shadow my-1 py-1 px-2 border-b-2 border-blue-700 hover:border-blue-800 rounded-lg items-center transition ease-in-out duration-150">
+    <b-icon icon="paperclip"></b-icon>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    props: {
+      type: {
+        type: String,
+        default: 'button',
+      },
+    }
+  }
+</script>
+<style scoped>
+  .bg-biru {
+    background-color: #1D7CBF!important;
+  }
+
+  .bg-oren {
+    background-color: #F7A31C;
+  }
+
+  .text-oren {
+    color: #F7A31C;
+    font-weight: 700;
+  }
+</style>
