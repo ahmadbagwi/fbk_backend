25f48a75c23175707970ffd606d8c97ceab5ba30
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-10-29 10:57:26 +0700

    update info, button-attach, date-picker

--- resources/js/Pages/Form/Laporan.vue
+++ resources/js/Pages/Form/Laporan.vue
@@ -1,181 +1,184 @@
 <template>
-    <app-layout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Pengajuan
-            </h2>
-        </template>
+  <app-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Laporan
+      </h2>
+    </template>
 
-        <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-              <jet-form-section @submitted="submitForm">
-                <template #title>
-                  Form Laporan {{ $page.auth.user.kegiatan }}
-                </template>
+    <div>
+      <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+        <jet-form-section @submitted="submitForm">
+          <template #title>
+            Form Laporan {{ $page.auth.user.kegiatan }}
+          </template>
 
-                <template #description>
-                  <p>Silahkan lengkapi data anda<br>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et aliquam libero. Donec iaculis commodo efficitur. Suspendisse aliquam mollis ipsum, eu dictum nunc fermentum a. Curabitur aliquam lacus nisl, ac rutrum lorem placerat vel. 
-                  </p>
-                </template>
+          <template #description>
+            <p>Silahkan isi tanggal terima dana, tanggal mulai dan tanggal selesai kegiatan. Kemudian unggah file laporan.<br>
+            </p>
+          </template>
 
-                <template #form>
-                    <div class="col-span-6 sm:col-span-4">
-                      <jet-label for="terima" value="Tanggal Terima Dana" />
-                      <jet-input id="terima" type="date" class="mt-1 block w-full" v-model="form.terima" />
-                      <jet-input-error :message="errors.terima" class="mt-2" />
-                    </div>
-
-                    <div class="col-span-3 sm:col-span-3">
-                      <jet-label for="mulai" value="Tanggal Pelaksanaan Kegiatan (mulai)" />
-                      <jet-input id="mulai" type="date" class="mt-1 block w-full" v-model="form.mulai" />
-                      <jet-input-error :message="errors.mulai" class="mt-2" />
-                    </div>
-
-                    <div class="col-span-3 sm:col-span-3">
-                      <jet-label for="selesai" value="Tanggal Pelaksanaan Kegiatan (selesai)" />
-                      <jet-input id="selesai" type="date" class="mt-1 block w-full" v-model="form.selesai" />
-                      <jet-input-error :message="errors.selesai" class="mt-2" />
-                    </div>
-
-                    <div class="col-span-6 sm:col-span-4">
-                      <div class="input-group">
-                        <jet-label for="laporan80" value="Laporan 80%" v-if="$page.auth.user.kegiatan=='FBK'"/>
-                        <jet-label for="laporan80" value="Laporan Awal" v-else />
-                        <div class="input-group-btn">
-                          <button class="flex ext-blue-600 border border-blue-600 hover:bg-gray-200 px-4 py-2 rounded" @click.prevent="openFileManager">
-                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-paperclip" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                              <path fill-rule="evenodd" d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0V3z"/>
-                            </svg>Pilih/Unggah
-                          </button>
-                        </div>
-                        <jet-input id="laporan80" type="text" class="mt-1 block w-full bg-gray-100" v-model="form.laporan80" readonly/>
-                        <jet-input-error :message="errors.laporan80" class="mt-2" />
-                      </div>
-                    </div>
-
-                    <div class="col-span-6 sm:col-span-4">
-                      <div class="input-group">
-                        <jet-label for="laporan20" value="Laporan Akhir 20%"  v-if="$page.auth.user.kegiatan=='FBK'"/>
-                        <jet-label for="laporan20" value="Laporan Akhir" v-else />
-                        <div class="input-group-btn">
-                          <button class="flex ext-blue-600 border border-blue-600 hover:bg-gray-200 px-4 py-2 rounded" @click.prevent="openFileManager2">
-                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-paperclip" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                              <path fill-rule="evenodd" d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0V3z"/>
-                            </svg>Pilih/Unggah
-                          </button>
-                        </div>
-                        <jet-input id="laporan20" type="text" class="mt-1 block w-full bg-gray-100" v-model="form.laporan20" readonly/>
-                        <jet-input-error :message="errors.laporan20" class="mt-2" />
-                      </div>
-                    </div>
-                    
-                </template>
-
-                <template #actions>
-                  <jet-action-message :on="form.recentlySuccessful" class="mr-3">
-                    <span v-if="errors">
-                      Error periksa kembali input anda
-                    </span>
-                    <span v-else>
-                      Tersimpan
-                    </span>
-                  </jet-action-message>
-                  <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Simpan
-                  </jet-button>
-                </template>
-              </jet-form-section>
-
-              <jet-section-border />
-
-              <!-- <delete-user-form class="mt-10 sm:mt-0" /> -->
+          <template #form>
+            <div class="col-span-6 sm:col-span-4">
+              <jet-label for="terima" value="Tanggal Terima Dana" />
+              <!-- <jet-input id="terima" type="date" class="mt-1 block w-full" v-model="form.terima" /> -->
+              <date-picker v-model="form.terima" type="date" format="DD-MM-YYYY" valueType="YYYY-MM-DD" class="mt-1 block w-full"></date-picker>
+              <jet-input-error :message="errors.terima" class="mt-2" />
             </div>
-        </div>
-    </app-layout>
+
+            <div class="col-span-3 sm:col-span-3">
+              <jet-label for="mulai" value="Tanggal Pelaksanaan Kegiatan (mulai)" />
+              <!-- <jet-input id="mulai" type="date" class="mt-1 block w-full" v-model="form.mulai" /> -->
+              <date-picker v-model="form.mulai" type="date" format="DD-MM-YYYY" valueType="YYYY-MM-DD" class="mt-1 block w-full"></date-picker>
+              <jet-input-error :message="errors.mulai" class="mt-2" />
+            </div>
+
+            <div class="col-span-3 sm:col-span-3">
+              <jet-label for="selesai" value="Tanggal Pelaksanaan Kegiatan (selesai)" />
+              <!-- <jet-input id="selesai" type="date" class="mt-1 block w-full" v-model="form.selesai" /> -->
+              <date-picker v-model="form.selesai" type="date" format="DD-MM-YYYY" valueType="YYYY-MM-DD" class="mt-1 block w-full"></date-picker>
+              <jet-input-error :message="errors.selesai" class="mt-2" />
+            </div>
+
+            <div class="col-span-6 sm:col-span-4">
+              <div class="input-group">
+                <jet-label for="laporan80" value="Laporan 80%" v-if="$page.auth.user.kegiatan=='FBK'"/>
+                <jet-label for="laporan80" value="Laporan Awal" v-else />
+                <div class="input-group-btn">
+                  <button-attach>
+                    <span @click.prevent="openFileManager">Pilih/Unggah</span>
+                  </button-attach>
+                </div>
+                <jet-input id="laporan80" type="text" class="mt-1 block w-full bg-gray-100" v-model="form.laporan80" readonly/>
+                <jet-input-error :message="errors.laporan80" class="mt-2" />
+              </div>
+            </div>
+
+            <div class="col-span-6 sm:col-span-4">
+              <div class="input-group">
+                <jet-label for="laporan20" value="Laporan Akhir 20%"  v-if="$page.auth.user.kegiatan=='FBK'"/>
+                <jet-label for="laporan20" value="Laporan Akhir" v-else />
+                <div class="input-group-btn">
+                  <button-attach>
+                    <span @click.prevent="openFileManager2">Pilih/Unggah</span>
+                  </button-attach>
+                </div>
+                <jet-input id="laporan20" type="text" class="mt-1 block w-full bg-gray-100" v-model="form.laporan20" readonly/>
+                <jet-input-error :message="errors.laporan20" class="mt-2" />
+              </div>
+            </div>
+
+          </template>
+
+          <template #actions>
+            <jet-action-message :on="form.recentlySuccessful" class="mr-3">
+              <span v-if="errors">
+                Error periksa kembali input anda
+              </span>
+              <span v-else>
+                Tersimpan
+              </span>
+            </jet-action-message>
+            <jet-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+              Simpan
+            </jet-button>
+          </template>
+        </jet-form-section>
+
+        <jet-section-border />
+
+        <!-- <delete-user-form class="mt-10 sm:mt-0" /> -->
+      </div>
+    </div>
+  </app-layout>
 </template>
 
 <script>
-    import AppLayout from './../../Layouts/AppLayout'
-    import JetButton from './../../Jetstream/Button'
-    import JetFormSection from './../../Jetstream/FormSection'
-    import JetInput from './../../Jetstream/Input'
-    import JetInputError from './../../Jetstream/InputError'
-    import JetLabel from './../../Jetstream/Label'
-    import JetActionMessage from './../../Jetstream/ActionMessage'
-    import JetSecondaryButton from './../../Jetstream/SecondaryButton'
-    import JetSectionBorder from './../../Jetstream/SectionBorder'
+  import AppLayout from './../../Layouts/AppLayout'
+  import JetButton from './../../Jetstream/Button'
+  import ButtonAttach from './../../Jetstream/ButtonAttach'
+  import JetFormSection from './../../Jetstream/FormSection'
+  import JetInput from './../../Jetstream/Input'
+  import JetInputError from './../../Jetstream/InputError'
+  import JetLabel from './../../Jetstream/Label'
+  import JetActionMessage from './../../Jetstream/ActionMessage'
+  import JetSecondaryButton from './../../Jetstream/SecondaryButton'
+  import JetSectionBorder from './../../Jetstream/SectionBorder'
+  import DatePicker from 'vue2-datepicker'
+  import 'vue2-datepicker/index.css'
 
-    export default {
-        props: {
-          message: '',
-          data: {},
-          errors: {}
-        },
+  export default {
+    props: {
+      message: '',
+      data: {},
+      errors: {}
+    },
 
-        components: {
-            AppLayout,
-            JetActionMessage,
-            JetButton,
-            JetFormSection,
-            JetInput,
-            JetInputError,
-            JetLabel,
-            JetSecondaryButton,
-            JetSectionBorder
-        },
+    components: {
+      AppLayout,
+      JetActionMessage,
+      JetButton,
+      ButtonAttach,
+      JetFormSection,
+      JetInput,
+      JetInputError,
+      JetLabel,
+      JetSecondaryButton,
+      JetSectionBorder,
+      DatePicker
+    },
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    terima: null,
-                    mulai: null,
-                    selesai: null,
-                    laporan80: null,
-                    laporan20: null,
-                }, {
-                    bag: 'submitForm',
-                    resetOnSuccess: false,
-                }),
+    data() {
+      return {
+        form: this.$inertia.form({
+          terima: null,
+          mulai: null,
+          selesai: null,
+          laporan80: null,
+          laporan20: null,
+        }, {
+          bag: 'submitForm',
+          resetOnSuccess: false,
+        }),
 
-            }
-        },
+      }
+    },
 
-        created() {
-          if (this.data !== null) {
-            this.form.terima = this.data.terima
-            this.form.mulai = this.data.mulai
-            this.form.selesai = this.data.selesai
-            this.form.laporan80 = this.data.laporan80
-            this.form.laporan20 = this.data.laporan20
-          }
-        },
+    created() {
+      if (this.data !== null) {
+        this.form.terima = this.data.terima
+        this.form.mulai = this.data.mulai
+        this.form.selesai = this.data.selesai
+        this.form.laporan80 = this.data.laporan80
+        this.form.laporan20 = this.data.laporan20
+      }
+    },
 
-        methods: {
-          submitForm() {
-            this.form.post('/laporan/store', {
-              preserveScroll: true
-            })
-          },
+    methods: {
+      submitForm() {
+        this.form.post('/laporan/store', {
+          preserveScroll: true
+        })
+      },
 
-          openFileManager () {
-            window.open(`/laravel-filemanager`, 'width=900,height=600')
-            var self = this
-            window.SetUrl = function (items) {
-              self.form.laporan80 = items[0].url
-            }
-            return false
-          },
+      openFileManager () {
+        window.open(`/laravel-filemanager`, 'width=900,height=600')
+        var self = this
+        window.SetUrl = function (items) {
+          self.form.laporan80 = items[0].url
+        }
+        return false
+      },
 
-          openFileManager2 () {
-            window.open(`/laravel-filemanager`, 'width=900,height=600')
-            var self = this
-            window.SetUrl = function (items) {
-              self.form.laporan20 = items[0].url
-            }
-            return false
-          },
+      openFileManager2 () {
+        window.open(`/laravel-filemanager`, 'width=900,height=600')
+        var self = this
+        window.SetUrl = function (items) {
+          self.form.laporan20 = items[0].url
+        }
+        return false
+      },
 
-        },
-    }
+    },
+  }
 </script>
--- resources/js/Pages/Form/Pengajuan.vue
+++ resources/js/Pages/Form/Pengajuan.vue
@@ -14,8 +14,8 @@
                 </template>
 
                 <template #description>
-                  <p>Silahkan lengkapi data anda<br>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et aliquam libero. Donec iaculis commodo efficitur. Suspendisse aliquam mollis ipsum, eu dictum nunc fermentum a. Curabitur aliquam lacus nisl, ac rutrum lorem placerat vel. 
+                  <p>Silahkan lengkapi formulir pengajuan<br>
+
                   </p>
                 </template>
 
@@ -37,7 +37,7 @@
                       <jet-input-error :message="errors.judul" class="mt-2" />
                     </div>
 
-                    <div class="col-span-6 sm:col-span-4">
+                    <div class="col-span-6 sm:col-span-5">
                       <jet-label for="konsep" value="Konsep" />
                       <wysiwyg  v-model="form.konsep"/>
                       <jet-input-error :message="errors.konsep" class="mt-2" />
@@ -45,23 +45,25 @@
 
                     <div class="col-span-3 sm:col-span-3">
                       <jet-label for="mulai" value="Tanggal Mulai" />
-                      <jet-input id="mulai" type="date" class="mt-1 block w-full" v-model="form.mulai" autocomplete="mulai" />
+                      <!-- <jet-input id="mulai" type="date" class="mt-1 block w-full" v-model="form.mulai" autocomplete="mulai" /> -->
+                      <date-picker v-model="form.mulai" type="date" format="DD-MM-YYYY" valueType="YYYY-MM-DD" class="mt-1 block w-full"></date-picker>
                       <jet-input-error :message="errors.mulai" class="mt-2" />
                     </div>
 
                     <div class="col-span-3 sm:col-span-3">
                       <jet-label for="selesai" value="Tanggal Selesai" />
-                      <jet-input id="selesai" type="date" class="mt-1 block w-full" v-model="form.selesai" autocomplete="selesai" />
+                      <!-- <jet-input id="selesai" type="date" class="mt-1 block w-full" v-model="form.selesai" autocomplete="selesai" /> -->
+                      <date-picker v-model="form.selesai" type="date" format="DD-MM-YYYY" valueType="YYYY-MM-DD" class="mt-1 block w-full"></date-picker>
                       <jet-input-error :message="errors.selesai" class="mt-2" />
                     </div>
 
-                    <div class="col-span-6 sm:col-span-4">
+                    <div class="col-span-6 sm:col-span-5">
                       <jet-label for="tahap1" value="Pelaksanaan Tahap 1" />
                       <wysiwyg  v-model="form.tahap1"/>
                       <jet-input-error :message="errors.tahap1" class="mt-2" />
                     </div>
 
-                    <div class="col-span-6 sm:col-span-4">
+                    <div class="col-span-6 sm:col-span-5">
                       <jet-label for="tahap2" value="Pelaksanaan Tahap 2" />
                       <wysiwyg  v-model="form.tahap2"/>
                       <jet-input-error :message="errors.tahap2" class="mt-2" />
@@ -77,11 +79,9 @@
                       <div class="input-group">
                         <jet-label for="rab" value="RAB" />
                         <div class="input-group-btn">
-                          <button class="flex ext-blue-600 border border-blue-600 hover:bg-gray-200 px-4 py-2 rounded" @click.prevent="openFileManager">
-                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-paperclip" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                              <path fill-rule="evenodd" d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0V3z"/>
-                            </svg>Pilih/Unggah
-                          </button>
+                          <button-attach>
+                            <span @click.prevent="openFileManager">Pilih/Unggah</span>
+                          </button-attach>
                         </div>
                         <jet-input id="rab" type="text" class="mt-1 block w-full bg-gray-100" v-model="form.rab" autocomplete="rab" readonly/>
                         <jet-input-error :message="errors.rab" class="mt-2" />
@@ -122,88 +122,93 @@
 </template>
 
 <script>
-    import AppLayout from './../../Layouts/AppLayout'
-    import JetButton from './../../Jetstream/Button'
-    import JetFormSection from './../../Jetstream/FormSection'
-    import JetInput from './../../Jetstream/Input'
-    import JetInputError from './../../Jetstream/InputError'
-    import JetLabel from './../../Jetstream/Label'
-    import JetActionMessage from './../../Jetstream/ActionMessage'
-    import JetSecondaryButton from './../../Jetstream/SecondaryButton'
-    import JetSectionBorder from './../../Jetstream/SectionBorder'
+  import AppLayout from './../../Layouts/AppLayout'
+  import JetButton from './../../Jetstream/Button'
+  import ButtonAttach from './../../Jetstream/ButtonAttach'
+  import JetFormSection from './../../Jetstream/FormSection'
+  import JetInput from './../../Jetstream/Input'
+  import JetInputError from './../../Jetstream/InputError'
+  import JetLabel from './../../Jetstream/Label'
+  import JetActionMessage from './../../Jetstream/ActionMessage'
+  import JetSecondaryButton from './../../Jetstream/SecondaryButton'
+  import JetSectionBorder from './../../Jetstream/SectionBorder'
+  import DatePicker from 'vue2-datepicker'
+  import 'vue2-datepicker/index.css'
 
-    export default {
-        props: {
-          message: '',
-          data: {},
-          errors: {}
-        },
+  export default {
+    props: {
+      message: '',
+      data: {},
+      errors: {}
+    },
 
-        components: {
-            AppLayout,
-            JetActionMessage,
-            JetButton,
-            JetFormSection,
-            JetInput,
-            JetInputError,
-            JetLabel,
-            JetSecondaryButton,
-            JetSectionBorder
-        },
+    components: {
+      AppLayout,
+      JetActionMessage,
+      JetButton,
+      ButtonAttach,
+      JetFormSection,
+      JetInput,
+      JetInputError,
+      JetLabel,
+      JetSecondaryButton,
+      JetSectionBorder,
+      DatePicker
+    },
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    jenis: null,
-                    judul: null,
-                    konsep: null,
-                    mulai: null,
-                    selesai: null,
-                    tahap1: null,
-                    tahap2: null,
-                    biaya: null,
-                    rab: null,
-                    video: null,
-                    status: null
-                }, {
-                    bag: 'submitForm',
-                    resetOnSuccess: false,
-                }),
+    data() {
+      return {
+        form: this.$inertia.form({
+          jenis: null,
+          judul: null,
+          konsep: null,
+          mulai: null,
+          selesai: null,
+          tahap1: null,
+          tahap2: null,
+          biaya: null,
+          rab: null,
+          video: null,
+          status: null
+        }, {
+          bag: 'submitForm',
+          resetOnSuccess: false,
+        }),
 
-            }
-        },
+      }
+    },
 
-        created() {
-          if (this.data !== null) {
-            this.form.jenis = this.data.jenis
-            this.form.judul = this.data.judul
-            this.form.konsep = this.data.konsep
-            this.form.mulai = this.data.mulai
-            this.form.selesai = this.data.selesai
-            this.form.tahap1 = this.data.tahap1
-            this.form.tahap2 = this.data.tahap2
-            this.form.biaya = this.data.biaya
-            this.form.rab = this.data.rab
-            this.form.video = this.data.video
-          }
-        },
-        
-        methods: {
-          submitForm() {
-            this.form.post('/pengajuan/store', {
-              preserveScroll: true
-            })
-          },
+    created() {
+      if (this.data !== null) {
+        this.form.jenis = this.data.jenis
+        this.form.judul = this.data.judul
+        this.form.konsep = this.data.konsep
+        this.form.mulai = this.data.mulai
+        this.form.selesai = this.data.selesai
+        this.form.tahap1 = this.data.tahap1
+        this.form.tahap2 = this.data.tahap2
+        this.form.biaya = this.data.biaya
+        this.form.rab = this.data.rab
+        this.form.video = this.data.video
+      }
+    },
 
-          openFileManager () {
-            window.open(`/laravel-filemanager`, 'width=900,height=600')
-            var self = this
-            window.SetUrl = function (items) {
-              self.form.rab = items[0].url
-            }
-            return false
-          },
+    methods: {
+      submitForm() {
+        this.form.post('/pengajuan/store', {
+          preserveScroll: true
+        })
+      },
 
-        },
-    }
+      openFileManager () {
+        window.open(`/laravel-filemanager`, 'width=900,height=600')
+        var self = this
+        window.SetUrl = function (items) {
+          self.form.rab = items[0].url
+        }
+        return false
+      },
+
+    },
+  }
 </script>
