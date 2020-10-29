63b8e9cd4303de3f2802da7a6daf626ad5153bb2
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-10-29 11:02:56 +0700

    npm run production 29-10-2020

--- resources/js/app.js
+++ resources/js/app.js
@@ -32,11 +32,11 @@ Vue.component('downloadExcel', JsonExcel)
 const app = document.getElementById('app');
 
 new Vue({
-    render: (h) =>
-        h(InertiaApp, {
-            props: {
-                initialPage: JSON.parse(app.dataset.page),
-                resolveComponent: (name) => require(`./Pages/${name}`).default,
-            },
-        }),
+  render: (h) =>
+  h(InertiaApp, {
+    props: {
+      initialPage: JSON.parse(app.dataset.page),
+      resolveComponent: (name) => require(`./Pages/${name}`).default,
+    },
+  }),
 }).$mount(app);
