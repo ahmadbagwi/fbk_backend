a37fbac3b8e52273df89516991907d47d29f30d9
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-12-18 11:20:10 +0700

    test font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
-
+        <!-- <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;600;700&display=swap" rel="stylesheet"> -->
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
