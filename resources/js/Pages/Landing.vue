c5574412ae489ff15f29d98c956f5f87ed899a75
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-12-18 11:19:59 +0700

    landingpage baru sesuai mockup

---
+++ resources/js/Pages/Landing.vue
@@ -0,0 +1,44 @@
+<template>
+  <fbk-layout>
+    <div class="py-0">
+      <div class="w-full">
+        <div class="bg-white overflow-hidden shadow-xl rounded shadow">
+          <Slider />
+          <Penerima />
+          <TextBg />
+          <Komite />
+          <News />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  </fbk-layout>
+</template>
+
+<script>
+  import FbkLayout from './../Layouts/FbkLayout'
+  import Slider from './../Components/Slider'
+  import Penerima from './../Components/Penerima'
+  import TextBg from './../Components/TextBg'
+  import Komite from './../Components/Komite'
+  import News from './../Components/News'
+  import Footer from './../Components/Footer'
+  export default {
+    components: {
+      FbkLayout,
+      Slider,
+      Penerima,
+      TextBg,
+      Komite,
+      News,
+      Footer
+    },
+    props: {
+    },
+    data() {
+      return {
+      
+      }
+    }
+  }
+</script>
