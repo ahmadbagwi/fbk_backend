2cf33390a933be22f59a5adb764078bc76c8b599
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-12-19 10:23:50 +0700

    relasi profil

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,9 @@ class User extends Authenticatable
     {
       return $this->hasOne('App\Models\Biodata');
     }
+
+    public function profil()
+    {
+        return $this->hasMany('App\Models\Profil');
+    }
 }
