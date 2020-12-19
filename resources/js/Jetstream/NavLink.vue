2f7ccf7f485d5a4f3e89fe68baeb4ce080b5ae7a
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-12-19 10:27:01 +0700

    update mx font size

--- resources/js/Jetstream/NavLink.vue
+++ resources/js/Jetstream/NavLink.vue
@@ -11,8 +11,8 @@
         computed: {
             classes() {
                 return this.active
-                            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-xl font-medium leading-5 text-white focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-                            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-xl font-medium leading-5 text-gray-100 hover:text-white hover:border-gray-300 focus:outline-none focus:text-white focus:border-gray-300 transition duration-150 ease-in-out'
+                            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-md font-medium leading-5 text-white focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+                            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-md font-medium leading-5 text-gray-100 hover:text-white hover:border-gray-300 focus:outline-none focus:text-white focus:border-gray-300 transition duration-150 ease-in-out'
             }
         }
     }
