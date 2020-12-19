407174294fa1bcf2e5d16dd2f3c9137a7b574d30
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-12-19 10:23:05 +0700

    list style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
 
 module.exports = {
     purge: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php', './resources/js/**/*.vue'],
@@ -13,7 +14,25 @@ module.exports = {
 
     variants: {
         opacity: ['responsive', 'hover', 'focus', 'disabled'],
+        borderColors: ["responsive", "hover", "focus", "group-hover"],
+        borderWidth: ['responsive', 'hover', 'focus'],
+        visibility: ["responsive", "group-hover", 'group-focus', 'active'],
+        display: ["responsive", "group-hover", "group-focus", 'active'],
     },
 
-    plugins: [require('@tailwindcss/ui')],
+    plugins: [
+        require('@tailwindcss/ui'),
+        plugin(function({ addBase, config }) {
+            addBase({
+              'ul': { 
+                listStyleType: config('theme.listStyleType.disc'),
+                padding: config('theme.padding.8'),
+              },
+              'ol': { 
+                listStyleType: config('theme.listStyleType.decimal'),
+                padding: config('theme.padding.8'),
+              },
+            })
+          })
+    ],
 };
