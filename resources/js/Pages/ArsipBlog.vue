2ac2cb489536eed2989a29fb327e97c4e3b926ba
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-12-20 13:47:56 +0700

    page arsip blog

---
+++ resources/js/Pages/ArsipBlog.vue
@@ -0,0 +1,43 @@
+<template>
+  <fbk-layout>
+    <div class="py-0">
+      <div class="w-full">
+        <div class="bg-white overflow-hidden shadow-xl rounded shadow">
+          <Slider />
+          <BlogGrid :blog="blog" />
+          <div class="p-4 block mx-center">
+            <inertia-link href="/">
+              <button class="flex w-24 max-w-24 text-white-700 border-green-400 bg-gray-100 hover:bg-gray-200 px-1 py-1 rounded shadow" >
+              &#8656; &nbsp;Kembali
+              </button>
+            </inertia-link>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  </fbk-layout>
+</template>
+
+<script>
+  import FbkLayout from './../Layouts/FbkLayout'
+  import Slider from './../Components/Slider'
+  import BlogGrid from './../Components/BlogGrid'
+  import Footer from './../Components/Footer'
+  export default {
+    components: {
+      FbkLayout,
+      Slider,
+      BlogGrid,
+      Footer
+    },
+    props: {
+      data: {},
+    },
+    data() {
+      return {
+        blog: this.data,
+      }
+    },
+  }
+</script>
