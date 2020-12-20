448b9be026e5d4c11624fdbb4a79c3fe78481969
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-12-20 16:56:44 +0700

    tambah judul tiap kategori

--- resources/js/Components/Penerima.vue
+++ resources/js/Components/Penerima.vue
@@ -6,9 +6,9 @@
       </h2>
     </div>
     <div class="grid grid-cols-3 gap-6 mt-10 px-10">
-      <div class="col-span-3 md:col-span-1 grid-penerima shadow-md rounded-lg p-8">
+      <div class="col-span-3 md:col-span-1 grid-penerima shadow-md rounded-lg p-4">
         <!-- <img class="rounded-full text-center mx-auto" :src="`/${list.foto_penerima}`"> -->
-        <div class="z-0 -mt-20 flex justify-center">
+        <div class="z-0 -mt-1 sm:-mt-20 flex justify-center">
           <splide :options="options">
             <splide-slide v-for="list in dokumentasi" :key="list.id" class=" mx-auto w-full">
               <img :src="list.foto_penerima" class="mx-auto rounded-full w-40">
@@ -16,6 +16,9 @@
           </splide>
         </div>
         <div class="text-gray-800 text-center text-sm px-1">
+          <h3 class="text-lg font-bold text-center py-4">
+            Dokumentasi Karya/Pengetahuan Maestro
+          </h3>
           <ul class="list-none px-0">
             <li v-for="list in dokumentasi" :key="list.id" class="border border-white bg-gray-100 p-1 hover:bg-gray-200 hover:text-blue-500">
               <inertia-link :href="`/profil/${list.id}`">{{ list.nama_penerima }}</inertia-link>
@@ -30,9 +33,9 @@
           </div>
         </div>
       </div>
-      <div class="col-span-3 md:col-span-1 grid-penerima shadow-md rounded-lg p-8">
+      <div class="col-span-3 md:col-span-1 grid-penerima shadow-md rounded-lg p-4">
         <!-- <img class="rounded-full text-center mx-auto" :src="`/${list.foto_penerima}`"> -->
-        <div class="z-0 -mt-20 flex justify-center">
+        <div class="z-0 -mt-1 sm:-mt-20 flex justify-center">
           <splide :options="options">
             <splide-slide v-for="list in penciptaan" :key="list.id" class=" mx-auto w-full">
               <img :src="list.foto_penerima" class="mx-auto rounded-full w-40">
@@ -40,6 +43,9 @@
           </splide>
         </div>
         <div class="text-gray-800 text-center text-sm px-1">
+          <h3 class="text-lg font-bold text-center py-4">
+            Penciptaan Karya Kreatif Inovatif
+          </h3>
           <ul class="list-none px-0">
             <li v-for="list in penciptaan" :key="list.id" class="border border-white bg-gray-100 p-1 hover:bg-gray-200 hover:text-blue-500">
               <inertia-link :href="`/profil/${list.id}`">{{ list.nama_penerima }}</inertia-link>
@@ -54,9 +60,9 @@
           </div>
         </div>
       </div>
-      <div class="col-span-3 md:col-span-1 grid-penerima shadow-md rounded-lg p-8">
+      <div class="col-span-3 md:col-span-1 grid-penerima shadow-md rounded-lg p-4">
         <!-- <img class="rounded-full text-center mx-auto" :src="`/${list.foto_penerima}`"> -->
-        <div class="z-0 -mt-20 flex justify-center">
+        <div class="z-0 -mt-1 sm:-mt-20 flex justify-center">
           <splide :options="options">
             <splide-slide v-for="list in pendayagunaan" :key="list.id" class=" mx-auto w-full">
               <img :src="list.foto_penerima" class="mx-auto rounded-full w-40">
@@ -64,6 +70,9 @@
           </splide>
         </div>
         <div class="text-gray-800 text-center text-sm px-1">
+          <h3 class="text-lg font-bold text-center py-4 px-2">
+            Pendayagunaan Ruang Publik
+          </h3>
           <ul class="list-none px-0">
             <li v-for="list in  pendayagunaan" :key="list.id" class="border border-white bg-gray-100 p-1 hover:bg-gray-200 hover:text-blue-500">
               <inertia-link :href="`/profil/${list.id}`">{{ list.nama_penerima }}</inertia-link>
@@ -110,7 +119,7 @@ export default {
         type: 'fade',
         arrows: false,
         pagination: false,
-        interval: 200,
+        interval: 2500,
         arrowPath: false,
         gap    : '1rem',
         autoplay: true,
