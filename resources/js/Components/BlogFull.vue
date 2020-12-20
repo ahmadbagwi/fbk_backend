bdb6b3e3c3a69e3671f4cfe9c8582a42983346a5
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-12-20 13:43:51 +0700

    comp blog full, blog grid

---
+++ resources/js/Components/BlogFull.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="w-full mx-auto">
+    <div class="grid grid-cols-3 gap-2 mt-10 px-10">
+      <div class="col-span-3 shadow-md rounded-lg p-1">
+        <div class="text-gray-800 text-center text-sm px-1">
+          <img :src="blog.featured" class="block w-full mx-auto">
+          <h3 v-html="blog.judul" class="text-2xl text-center font-bold"></h3>
+          <div class="p-2 text-md">
+            <span v-html="blog.konten"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="p-4 block mx-center">
+      <inertia-link href="/">
+        <button class="flex w-24 max-w-24 text-white-700 border-green-400 bg-gray-100 hover:bg-gray-200 px-1 py-1 rounded shadow" >
+        &#8656; &nbsp;Kembali
+        </button>
+      </inertia-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  
+  },
+  props: {
+    blog: {}
+  },
+  data () {
+    return {
+      data: [
+        {id: 1, img: '/storage/profile.jpeg', text: 'Lorem Ipsum 1'},
+        {id: 2, img: '/storage/profile.jpeg', text: 'Lorem Ipsum 2'},
+        {id: 3, img: '/storage/profile.jpeg', text: 'Lorem Ipsum 3'},
+      ],
+    }
+  }
+}
+</script>
---
+++ resources/js/Components/BlogGrid.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="w-full mx-auto">
+    <div class="title text-center py-4">
+      <h2 class="text-3xl">
+        <inertia-link href="/arsip/blog/blog">
+          -NEWS <span class="font-bold">POOL-</span>
+        </inertia-link>
+      </h2>
+    </div>
+    <div class="grid grid-cols-3 gap-2 mt-1 px-10">
+      <div v-for="list in blog" :key="list.id" class="col-span-3 md:col-span-1 shadow-md rounded-lg p-1">
+        <div class="text-gray-800 text-center text-sm px-1">
+          <inertia-link :href="`/blog/${list.slug}`">
+            <img :src="list.featured" class="block w-full mx-auto">
+            <h3 v-html="list.judul" class="text-center font-bold"></h3>
+            <div class="p-2">
+              <span v-html="list.konten"></span>
+            </div>
+          </inertia-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  
+  },
+  props: {
+    blog: {}
+  },
+  data () {
+    return {
+      data: [
+        {id: 1, img: '/storage/profile.jpeg', text: 'Lorem Ipsum 1'},
+        {id: 2, img: '/storage/profile.jpeg', text: 'Lorem Ipsum 2'},
+        {id: 3, img: '/storage/profile.jpeg', text: 'Lorem Ipsum 3'},
+      ],
+    }
+  }
+}
+</script>
