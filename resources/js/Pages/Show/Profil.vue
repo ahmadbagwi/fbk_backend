84a5f0273cf85fd628992d7f0c51b25c418604ce
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-12-20 13:50:39 +0700

    show profil h3 pindah ke comp enerimaGrid

--- resources/js/Pages/Show/Profil.vue
+++ resources/js/Pages/Show/Profil.vue
@@ -4,6 +4,10 @@
       <div class="w-full">
         <div class="bg-white overflow-hidden shadow-xl rounded shadow">
           <Profil :profil="profil"/>
+          <h3 class="text-2xl text-gray-700 text-center font-semibold mx-1">
+            Penerima Lainnya
+          </h3>
+          <PenerimaGrid :penerimaAcakGrid="penerimaAcakGrid" />
           <Footer />
         </div>
       </div>
@@ -14,20 +18,24 @@
 <script>
   import FbkLayout from './../../Layouts/FbkLayout'
   import Profil from './../../Components/Profil'
+  import PenerimaGrid from './../../Components/PenerimaGrid'
   import Footer from './../../Components/Footer'
   export default {
     components: {
       FbkLayout,
       Profil,
+      PenerimaGrid,
       Footer
     },
     props: {
       data: {},
+      penerimaAcak: {},
     },
     data() {
       return {
-        profil: this.data
+        profil: this.data,
+        penerimaAcakGrid: this.penerimaAcak
       }
-    }
+    },
   }
 </script>
