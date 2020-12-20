9c95f4fb84d53a064a3fbd33a1e3095cb70c4a79
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-12-20 14:54:05 +0700

    ganti slider dengan heading

--- resources/js/Pages/ArsipBlog.vue
+++ resources/js/Pages/ArsipBlog.vue
@@ -3,7 +3,23 @@
     <div class="py-0">
       <div class="w-full">
         <div class="bg-white overflow-hidden shadow-xl rounded shadow">
-          <Slider />
+          <div class="z-30 relative opacity-85 p-2 px-4 sm:p-2 flex justify-between">
+            <inertia-link href="/">
+              <div class="text-white flex">
+                <img class="h-10 md:h-16" src="/storage/kemdikbud-putih.png">
+                <div class="flex-column">
+                  <div class="text-sm sm:text-2xl font-bold">Fasilitasi Bidang</div> 
+                  <div class="text-md sm:text-3xl font-bold leading-4">Kebudayaan</div>
+                </div>
+              </div>
+            </inertia-link>
+            <div class="flex p-1 sm:p-4">
+              <!-- <input type="text" class="w-28 sm:w-full rounded p-1 shadow" placeholder="Cari"> -->
+            </div>
+          </div>
+          <div class="z-0 -mt-20">
+              <img :src="`/storage/heading-1.JPG`">
+          </div>
           <BlogGrid :blog="blog" />
           <div class="p-4 block mx-center">
             <inertia-link href="/">
