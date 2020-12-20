dea9becb89fa4bd25ebf936c0cd7a271fcce20ae
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-12-20 16:56:32 +0700

    gunakan slug sebagai id untuk dipanggil

--- resources/js/Components/KomiteGrid.vue
+++ resources/js/Components/KomiteGrid.vue
@@ -3,7 +3,7 @@
     <div class="grid grid-cols-3 gap-2 mt-10 px-10">
       <div v-for="list in komite" :key="list.id" class="col-span-3 md:col-span-1 shadow-md rounded-lg p-1">
         <div class="text-gray-800 text-center text-sm px-1">
-          <img :src="list.featured" class="block rounded-full w-40 mx-auto">
+          <img :src="list.featured" class="block rounded-full w-40 mx-auto" :id="list.slug">
           <h3 v-html="list.judul" class="text-center font-bold"></h3>
           <div class="p-2">
             <span v-html="list.konten"></span>
