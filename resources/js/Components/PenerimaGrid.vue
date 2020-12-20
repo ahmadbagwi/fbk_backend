031a41c4e56cfce8d772e49f4081518c3cb44721
ahmadbagwi <ahmadbagwi.id@gmail.com>
2020-12-20 13:46:36 +0700

    data dinamis penerima grid

--- resources/js/Components/PenerimaGrid.vue
+++ resources/js/Components/PenerimaGrid.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="w-full mx-auto my-8">
     <div class="grid grid-cols-3 gap-6 px-10">
-      <div class="col-span-3 md:col-span-1 grid-penerima shadow-md rounded-lg p-8" v-for="list in penerima.dokumentasi" :key="list.id">
-        <img class="rounded-full text-center mx-auto" :src="`/${list.foto_penerima}`">
+      <div class="col-span-3 md:col-span-1 grid-penerima shadow-md rounded-lg p-8" v-for="list in penerimaAcakGrid" :key="list.id">
+        <img :src="`/${list.foto_penerima}`" class="w-40 rounded-full text-center mx-auto">
         <div class="text-gray-800 text-center text-sm px-1">
           <ul class="list-none">
-            <li><inertia-link :href="`/profil/${list.id}`">{{ list.nama_project }}</inertia-link></li>
+            <li><inertia-link :href="`/profil/${list.id}`">{{ list.nama_penerima }}</inertia-link></li>
           </ul>
         </div>
       </div>
@@ -20,7 +20,7 @@ export default {
   
   },
   props: {
-    penerima: {}
+    penerimaAcakGrid: {}
   },
   data () {
     return {
