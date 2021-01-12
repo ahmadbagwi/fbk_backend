c10c27363651978a056a06cb40ec6e409b96ce17
ahmadbagwi <ahmadbagwi.id@gmail.com>
2021-01-12 10:53:54 +0700

    update konten text-bg

--- resources/js/Components/TextBg.vue
+++ resources/js/Components/TextBg.vue
@@ -14,19 +14,19 @@
           <h3 class="text-lg font-semibold text-gray-200">
             Latar Belakang
           </h3>
-          <p class="text-gray-100">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta felis est, non varius velit placerat id.
+          <p class="text-gray-100 break-normal text-sm">
+            UUD 1945 dalam Pasal 32, yaitu “Pemerintah memajukan kebudayaan nasional Indonesia”, menekankan adanya keterlibatan langsung oleh pemerintah selaku penyusun kebijakan di bidang kebudayaan. Melalui Kongres Kebudayaan Indonesia (KKI) yang diselenggarakan pada tahun 2018, disepakati 7 (tujuh) agenda strategis Pemajuan Kebudayaan guna memajukan kebudayaan di Indonesia. Fasilitasi Bidang Kebudayaan (FBK) menjadi salah satu program yang dapat menjalankan amanat ketujuh agenda tersebut karena dampaknya dapat dimanfaatkan langsung oleh masyarakat, terutama dalam upaya pemajuan kebudayaan di Indonesia
           </p>
         </div>
         <div class="p-2 border-b-2 border-gray-100">
           <h3 class="text-lg font-semibold text-gray-200">
             Strategi Kebudayaan
           </h3>
-          <p class="text-gray-100">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta felis est, non varius velit placerat id.
-          </p>
+          <p class="text-gray-100 break-normal text-sm">
+            “Indonesia Bahagia berlandaskan keanekaragaman budaya yang mencerdaskan, mendamaikan dan menyejahterakan rakyat Indonesia seluruhnya” disepakati sebagai visi pemajuan kebudayaan 20 tahun ke depan. Oleh karena itu dalam penyusunannya, Strategi Pemajuan Kebudayaan dilandaskan kepada Trisakti, yakni asas berdaulat dalam politik, mandiri dalam ekonomi, dan berkepribadian dalam kebudayaan. FBK menjadi salah satu upaya yang dianggap dapat menjadi wadah untuk menjalankan amanat tujuh agenda strategis kebudayaan, yang menempatkan masyarakat sebagai pemilik dan penggerak kebudayaan nasional. Masyarakat adalah unsur yang paling utama untuk memajukan ekosistem kebudayaan di daerahnya masing-masing, hingga nantinya berujung ke pemajuan kebudayaan secara nasional.
+          </p> 
         </div>
-        <div class="p-2 border-b-2 border-gray-100">
+        <!-- <div class="p-2 border-b-2 border-gray-100">
           <h3 class="text-lg font-semibold text-gray-200">
             Pengumuman
           </h3>
@@ -41,7 +41,7 @@
           <p class="text-gray-100">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta felis est, non varius velit placerat id.
           </p>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
@@ -64,5 +64,6 @@ export default {
   .bg-text {
     background-image: url('/storage/bg-text.png');
     background-position: center;
+    background-size: cover;
   }
 </style>
