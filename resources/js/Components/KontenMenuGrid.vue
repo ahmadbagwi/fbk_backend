bd661b311cc914e11caeab4411c5831810971815
ahmadbagwi <ahmadbagwi.id@gmail.com>
2021-01-12 13:46:59 +0700

    role baru (konten) dashboard tersendiri

---
+++ resources/js/Components/KontenMenuGrid.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="flex flex-row h-full w-auto">
+      <div class="py-4 px-4 md:py-20 text-gray-700 flex md:flex-1 grid grid-cols-4 gap-6 md:gap-4">
+        <div class="col-span-2 md:col-span-1 p-4 w-40 rounded-md hover:shadow-none cursor-pointer shadow-md bg-gray-300 flex-column mx-auto border-2 border-gray-400">
+          <inertia-link href="/admin/profil">
+            <div class="icon text-center text-4xl md:text-6xl font-bold">
+              &#128195;
+            </div>
+            <div class="judul text-center text-md md:text-2xl font-bold">
+              Profil
+            </div>
+          </inertia-link>
+        </div>
+        <div class="col-span-2 md:col-span-1 p-4 w-40 rounded-md hover:shadow-none cursor-pointer shadow-md bg-gray-300 flex-column mx-auto border-2 border-gray-400">
+          <inertia-link href="/admin/blog">
+            <div class="icon text-center text-4xl md:text-6xl font-bold">
+              &#128203;
+            </div>
+            <div class="judul text-center text-md md:text-2xl font-bold">
+              Blog
+            </div>
+          </inertia-link>
+        </div>
+        <div class="col-span-2 md:col-span-1 p-4 w-40 rounded-md hover:shadow-none cursor-pointer shadow-md bg-gray-300 flex-column mx-auto border-2 border-gray-400">
+          <inertia-link href="/filemanager">
+            <div class="icon text-center text-4xl md:text-6xl font-bold">
+              &#9995;
+            </div>
+            <div class="judul text-center text-md md:text-2xl font-bold">
+              File Manager
+            </div>
+          </inertia-link>
+        </div>
+        <div class="col-span-2 md:col-span-1 p-4 w-40 rounded-md hover:shadow-none cursor-pointer shadow-md bg-gray-300 flex-column mx-auto border-2 border-gray-400">
+          <inertia-link href="/user/profile">
+            <div class="icon text-center text-md md:text-4xl md:text-6xl font-bold">
+              &#10067;
+            </div>
+            <div class="judul text-center text-2xl font-bold">
+              Akun
+            </div>
+          </inertia-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ resources/js/Pages/KontenDashboard.vue
@@ -0,0 +1,40 @@
+<template>
+  <KontenAppLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl .bg-green-400 leading-tight">
+        <h1>Selamat Datang Admin</h1>
+      </h2>
+    </template>
+    <fade-transition origin="center" mode="out-in" :duration="250">
+      <div class="py-4">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-1 md:py-4 md:px-12">
+            <KontenMenuGrid />
+          </div>
+        </div>
+      </div>
+    </fade-transition>
+  </KontenAppLayout>
+</template>
+
+<script>
+  import KontenAppLayout from './../Layouts/KontenAppLayout'
+  import KontenMenuGrid from './../Components/KontenMenuGrid'
+  import {FadeTransition} from 'vue2-transitions'
+
+  export default {
+    components: {
+      KontenAppLayout,
+      KontenMenuGrid,
+      FadeTransition
+    },
+    props: {
+      user: {}
+    },
+    data() {
+      return {
+      
+      }
+    }
+  }
+</script>
