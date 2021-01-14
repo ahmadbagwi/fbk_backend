d3859c7c27fff96795f2444015e21a917b30f624
ahmadbagwi <ahmadbagwi.id@gmail.com>
2021-01-14 19:00:55 +0700

    grid icon baru

--- resources/js/Components/Profil.vue
+++ resources/js/Components/Profil.vue
@@ -20,32 +20,64 @@
     <div class="p-4 block">
       <div class="flex-column mx-auto text-center pb-2 border-b border-gray-500">
         <img :src="`${profil.foto_penerima}`" class="rounded-full shadow w-40 mx-auto">
-        <div class="font-bold text-2xl">&#128195; {{profil.nama_project}}</div>
-        <div class="font-bold text-2xl">&#128203; {{profil.nama_penerima}}</div>
+        <div class="font-bold text-2xl my-1">&#128195; {{profil.nama_project}}</div>
+        <div class="font-bold text-2xl my-1">&#128203; {{profil.nama_penerima}}</div>
       </div>
-      <div class="flex justify-center my-2">
-        <div class="p-2 mx-1 rounded bg-green-500 shadow text-gray-200">
+      <div class="w-full mx-auto my-2">
+        <div class="grid grid-cols-3 gap-4">
+          <div class="col-span-3 md:col-span-1 p-2">
+            <div class="flex flex-col">
+              <div class="p-4 bg-blue-300 my-1 flex">
+                <img src="/storage/kategori.svg" class="text-white w-20 mr-2">
+                <span class="text-white font-bold flex items-center">{{ profil.kategori }}</span>
+              </div>
+              <div class="p-4 bg-blue-200 my-1 flex">
+                <img src="/storage/diajukan.svg" class="text-white w-20 mr-2">
+                <span class="text-white font-bold flex items-center">{{ diajukan }}</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-3 md:col-span-1 p-2">
+            <div class="flex flex-col">
+              <div class="p-4 bg-blue-400 my-1 flex">
+                <img src="/storage/waktu.svg" class="text-white w-20 mr-2">
+                <span class="text-white font-bold flex items-center">{{ profil.waktu }}</span>
+              </div>
+              <div class="p-4 bg-pink-300 my-1 flex">
+                <img src="/storage/disetujui.svg" class="text-white w-20 mr-2">
+                <span class="text-white font-bold flex items-center">{{ disetujui }}</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-3 md:col-span-1 bg-red-500 m-3 p-8 flex">
+            <img src="/storage/email.svg" class="text-white w-20 mr-2">
+                <span class="text-white font-bold flex items-center">{{ profil.kontak }}</span>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="flex flex-wrap justify-center my-2">
+        <div class="p-2 mx-1 rounded bg-green-500 shadow text-gray-200 my-1">
           &#128194; {{ profil.kategori }}
         </div>
-        <div class="p-2 mx-1 rounded bg-blue-500 shadow text-gray-200">
+        <div class="p-2 mx-1 rounded bg-blue-500 shadow text-gray-200 my-1">
           &#128197; {{ profil.waktu }}
         </div>
-        <div class="p-2 mx-1 rounded bg-red-500 shadow text-gray-200">
+        <div class="p-2 mx-1 rounded bg-red-500 shadow text-gray-200 my-1">
           &#9993; {{ profil.kontak }}
         </div>
       </div>
-      <div class="flex justify-center mx-2">
-        <span v-if="diajukan != '0'" class="bg-blue-500 text-gray-100 rounded p-2 mx-1">
+      <div class="flex flex-wrap justify-center mx-2">
+        <span v-if="diajukan != '0'" class="bg-blue-500 text-gray-100 rounded p-2 mx-1 my-1">
           Diajukan {{ diajukan }}
         </span>
-        <span v-if="disetujui != '0'" class="bg-green-500 text-gray-100 rounded p-2 mx-1">
+        <span v-if="disetujui != '0'" class="bg-green-500 text-gray-100 rounded p-2 mx-1 my-1">
           Disetujui {{ disetujui }}
         </span>
-      </div>
+      </div> -->
       <div class="grid grid-cols-4 gap-6 md:p-8 break-normal">
         <div class="col-span-4 md:col-span-4 my-4 break-normal pb-4 border-b border-gray-500">
           <h2 class="text-2xl text-gray-700">
-            Deskripsi
+            Profil Kegiatan
           </h2>
           <p class="break-normal">
             <span v-html="profil.deskripsi" class="break-normal"></span>
@@ -62,8 +94,31 @@
       </div>
     </div>
     <div class="p-4 block">
-      <div class="flex justify-center">
-        <span v-if="profil.media_sosial" class="bg-gray-300 rounded p-2 shadow hover:shadow-none text-gray-600">{{ profil.media_sosial }}</span>
+      <div class="grid grid-cols-4 gap-4">
+        <span v-if="url_facebook != ''" class="col-span-4 md:col-span-1 mx-2">
+          <a :href="url_facebook" target="_blank" class="flex">
+            <img src="/storage/facebook.png" class="w-8">
+            <span class="text-md md:text-lg text-gray-600 mx-2">{{ akun_facebook }}</span>
+          </a>
+        </span>
+        <span v-if="url_twitter != ''" class="col-span-4 md:col-span-1 mx-2">
+          <a :href="url_twitter" target="_blank" class="flex">
+            <img src="/storage/twitter.png" class="w-8">
+            <span class="text-md md:text-lg text-gray-600 mx-2">{{ akun_twitter }}</span>
+          </a>
+        </span>
+        <span v-if="url_instagram != ''" class="col-span-4 md:col-span-1 mx-2">
+          <a :href="url_instagram" target="_blank" class="flex">
+            <img src="/storage/instagram.png" class="w-8">
+            <span class="text-md md:text-lg text-gray-600 mx-2">{{ akun_instagram }}</span>
+          </a>
+        </span>
+        <span v-if="url_youtube != ''" class="col-span-4 md:col-span-1 mx-2">
+          <a :href="url_youtube" target="_blank" class="flex">
+            <img src="/storage/youtube.png" class="w-8">
+            <span class="text-md md:text-lg text-gray-600 mx-2">Youtube</span>
+          </a>
+        </span>
       </div>
     </div>
     <div class="p-4 block">
@@ -86,11 +141,31 @@ export default {
   },
   data () {
     return {
+      akun_facebook: '',
+      akun_twitter: '',
+      akun_instagram: '',
+      url_facebook: '',
+      url_twitter: '',
+      url_instagram: '',
+      url_youtube: '',
       data: [
         {id: 1, img: '/storage/profile.jpeg', text: 'Lorem Ipsum 1'},
         {id: 2, img: '/storage/profile.jpeg', text: 'Lorem Ipsum 2'},
         {id: 3, img: '/storage/profile.jpeg', text: 'Lorem Ipsum 3'},
       ],
+    }
+  },
+  created () {
+    if(this.profil.media_sosial != '') {
+      let array_medsos = []
+      array_medsos.push(this.profil.media_sosial.split(","))
+      this.akun_facebook = array_medsos[0][0]
+      this.url_facebook = array_medsos[0][1]
+      this.akun_twitter = array_medsos[0][2]
+      this.url_twitter = array_medsos[0][3]
+      this.akun_instagram = array_medsos[0][4]
+      this.url_instagram = array_medsos[0][5]
+      this.url_youtube = array_medsos[0][6]
     }
   },
   computed: {
