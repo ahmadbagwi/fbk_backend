30722ea81cd7134c8c2f7e243be79b7f52cee80a
ahmadbagwi <ahmadbagwi.id@gmail.com>
2021-02-04 01:07:00 +0700

    clean code

--- app/Models/Biodata.php
+++ app/Models/Biodata.php
@@ -11,7 +11,20 @@ class Biodata extends Model
     use HasFactory, SoftDeletes;
 
     protected $table = 'biodata';
-    protected $fillable = ['kategori', 'user_id', 'nama_pengusul', 'nama_penanggungjawab', 'ktp', 'kemenkumham', 'akta', 'npwp', 'alamat', 'provinsi', 'kota', 'telp', 'email' ];
+    protected $fillable = [
+        'kategori',
+        'user_id',
+        'nama_pengusul',
+        'nama_penanggungjawab',
+        'ktp',
+        'kemenkumham',
+        'akta',
+        'npwp',
+        'alamat',
+        'provinsi',
+        'kota',
+        'telp',
+        'email' ];
 
     public function user()
     {
