82656fc2f2f0334a611b1fbb01e69831b4062fc2
ahmadbagwi <ahmadbagwi.id@gmail.com>
2021-02-07 22:19:37 +0700

    fix tidak required untuk perorangan

--- app/Http/Requests/BiodataRequest.php
+++ app/Http/Requests/BiodataRequest.php
@@ -26,16 +26,16 @@ class BiodataRequest extends FormRequest
         return [
             'kategori' => 'required',
             'nama_pengusul' => 'required|max:200',
-            'nama_penanggungjawab' => 'required|max:200',
-            'ktp' => 'required|max:300',
-            'kemenkumham' => 'required|max:300',
-            'akta' => 'required|max:300',
-            'npwp' => 'required|max:300',
-            'alamat' => 'required|max:300',
-            'provinsi' => 'required|max:300',
-            'kota' => 'required|max:300',
-            'telp' => 'required|max:15',
-            'email' => 'required|email|max:100'
+            // 'nama_penanggungjawab' => 'required|max:200',
+            // 'ktp' => 'required|max:300',
+            // 'kemenkumham' => 'required|max:300',
+            // 'akta' => 'required|max:300',
+            // 'npwp' => 'required|max:300',
+            // 'alamat' => 'required|max:300',
+            // 'provinsi' => 'required|max:300',
+            // 'kota' => 'required|max:300',
+            // 'telp' => 'required|max:15',
+            // 'email' => 'required|email|max:100'
         ];
     }
 
@@ -44,16 +44,16 @@ class BiodataRequest extends FormRequest
         return [
             'kategori.required' => 'Kategori harus dipilih',
             'nama_pengusul.required' => 'Nama pengusul harus diisi maksimal 200 karakter',
-            'nama_penanggungjawab.required' => 'Nama penanggungjawab harus diisi maksimal 200 karakter',
-            'ktp.required' => 'File KTP harus dipilih/diunggah',
-            'kemenkumham.required' => 'File SK Kemenkumham harus dipilih/diunggah',
-            'akta.required' => 'File Akta harus dipilih/diunggah',
-            'npwp.required' => 'NPWP',
-            'alamat.required' => 'Alamat harus diisi maksimal 300 karakter',
-            'provinsi.required' => 'Provinsi harus diisi maksimal 300 karakter',
-            'kota.required' => 'Kota harus diisi maksimal 300 karakter',
-            'telp.required' => 'Telp harus diisi maksimal 15 karakter',
-            'email.required' => 'Email harus diisi maksimal 100 karakter'
+            // 'nama_penanggungjawab.required' => 'Nama penanggungjawab harus diisi maksimal 200 karakter',
+            // 'ktp.required' => 'File KTP harus dipilih/diunggah',
+            // 'kemenkumham.required' => 'File SK Kemenkumham harus dipilih/diunggah',
+            // 'akta.required' => 'File Akta harus dipilih/diunggah',
+            // 'npwp.required' => 'NPWP',
+            // 'alamat.required' => 'Alamat harus diisi maksimal 300 karakter',
+            // 'provinsi.required' => 'Provinsi harus diisi maksimal 300 karakter',
+            // 'kota.required' => 'Kota harus diisi maksimal 300 karakter',
+            // 'telp.required' => 'Telp harus diisi maksimal 15 karakter',
+            // 'email.required' => 'Email harus diisi maksimal 100 karakter'
         ];
     }
 }
