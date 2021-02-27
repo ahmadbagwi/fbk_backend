0c771cef1742180e67f92999ee1fc68348015c54
ahmad-x <ahmadbagwi.id@gmail.com>
2021-02-27 10:48:56 +0700

    user with periode

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller
 
     public function user_admin ()
     {
-        $data = User::all();
+        $data = User::with('periode')->get();
         return response()->json([
             'data' => $data
         ]);
