56156c54ca1db0ac8d75fa10d6d0a47cb0d7f90b
ahmadbagwi <ahmadbagwi.id@gmail.com>
2021-03-18 19:10:51 +0700

    install maatwebsite/excel

---
+++ app/Exports/BiodataPengajuanExport.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\BiodataPengajuan;
+use Maatwebsite\Excel\Concerns\FromCollection;
+
+class BiodataPengajuanExport implements FromCollection
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        return BiodataPengajuan::select(
+        'user_id',
+        'kategori_pengusul',
+        'nama_pengusul',
+        'telp',
+        'email',
+        'alamat',
+        'kota',
+        'provinsi',
+        'kategori_kegiatan',
+        'judul_kegiatan',
+        'deskripsi_kegiatan',
+        'durasi_pelaksanaan',
+        'hasil_kegiatan',
+        'penerima_manfaat',
+        'biaya_diajukan',
+        'pertanyaan',
+        'rab',
+        'status'
+        )->get();
+    }
+}
