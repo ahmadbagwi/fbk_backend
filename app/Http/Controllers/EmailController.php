13e4e0d7db910e68a94a7007bfb2a09c38a2525e
ahmad-x <ahmadbagwi.id@gmail.com>
2021-05-20 17:50:24 +0700

    +method email seleksi substansi administrasi

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -69,4 +69,74 @@ class EmailController extends Controller
 		$data = BiodataPengajuan::where('user_id', auth()->user()->id)->orderBy('created_at', 'desc')->first();
         return view('email.konfirmasi', ['data' => $data]);
     }
+
+    public function administrasi_lulus_view ()
+	{
+		$data = BiodataPengajuan::where('status', 'lulus')->select('id', 'email', 'status')->first();
+        return view('email.administrasi', ['data' => $data]);
+    }
+
+    public function administrasi_lulus ()
+    {
+    	$data_lulus = BiodataPengajuan::where('status', 'lulus')->select('id', 'email', 'status')->get();
+		foreach ($data_lulus as $data) {
+			Mail::send('email.administrasi', ['data' => $data], function($message) use ($data) {
+				$message->to($data->email)->subject('Pengumuman Seleksi Substansi FBK 2021');
+				$message->from('admin@fbk.id', 'Admin FBK ID');
+			});
+			if (count(Mail::failures()) > 0){
+			    $log = new Email;
+				$log->user_id = auth()->user()->id;
+				$log->tanggal = date('Y-m-d H:m:s');
+				$log->keterangan = 'notifikasi seleksi substansi '.$data->id.' '.$data->email.' '.$data->status;
+				$log->status = 'gagal';
+				$log->save();
+				echo 'notifikasi seleksi substansi '.$data->id.' tujuan '.$data->email.' gagal<br>';
+			} else {
+				$log = new Email;
+				$log->user_id = auth()->user()->id;
+				$log->tanggal = date('Y-m-d H:m:s');
+				$log->keterangan = 'notifikasi seleksi substansi '.$data->id.' '.$data->email.' '.$data->status;
+				$log->status = 'terkirim';
+				$log->save();
+				echo 'notifikasi seleksi substansi '.$data->id.' tujuan '.$data->email.' terkirim<br>';
+			}
+			sleep(2);
+		}
+    }
+
+    public function administrasi_tidak_lulus_view ()
+	{
+		$data = BiodataPengajuan::where('status', 'tidak lulus')->select('id', 'email', 'status')->first();
+        return view('email.administrasi', ['data' => $data]);
+    }
+
+    public function administrasi_tidak_lulus ()
+    {
+    	$data_lulus = BiodataPengajuan::where('status', 'tidak lulus')->select('id', 'email', 'status')->get();
+		foreach ($data_lulus as $data) {
+			Mail::send('email.administrasi', ['data' => $data], function($message) use ($data) {
+				$message->to($data->email)->subject('Pengumuman Seleksi Substansi FBK 2021');
+				$message->from('admin@fbk.id', 'Admin FBK ID');
+			});
+			if (count(Mail::failures()) > 0){
+			    $log = new Email;
+				$log->user_id = auth()->user()->id;
+				$log->tanggal = date('Y-m-d H:m:s');
+				$log->keterangan = 'notifikasi seleksi substansi '.$data->id.' '.$data->email.' '.$data->status;
+				$log->status = 'gagal';
+				$log->save();
+				echo 'notifikasi seleksi substansi '.$data->id.' tujuan '.$data->email.' gagal<br>';
+			} else {
+				$log = new Email;
+				$log->user_id = auth()->user()->id;
+				$log->tanggal = date('Y-m-d H:m:s');
+				$log->keterangan = 'notifikasi seleksi substansi '.$data->id.' '.$data->email.' '.$data->status;
+				$log->status = 'terkirim';
+				$log->save();
+				echo 'notifikasi seleksi substansi '.$data->id.' tujuan '.$data->email.' terkirim<br>';
+			}
+			sleep(2);
+		}
+    }
 }
